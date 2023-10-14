ca081e94d9258a408d421d7e364fb5b34321b8d6
ZETY10 <125837973+ZETY10@users.noreply.github.com>
2023-10-14 15:53:00 -0600

    subido

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+let MAIN;
+let MODAL_POST;
+let BTN_SHOW_POST;
+let BTN_CANCEL_POST;
+
+window.addEventListener('load', () => {
+    MAIN = document.querySelector('#main');
+    MODAL_POST = document.querySelector('#modal-post-section');
+    BTN_SHOW_POST = document.querySelector('#btn-upload-post');
+    BTN_SHOW_POST.addEventListener('click', showPostModal);
+    BTN_CANCEL_POST = document.querySelector('#btn-post-cancel');
+    BTN_CANCEL_POST.addEventListener('click', closePostModal);
+});
+
+
+const showPostModal = () => {
+    MAIN.style.display = 'none';
+    MODAL_POST.style.display = 'block';
+    setTimeout(() => {
+        MODAL_POST.style.transform = 'translateY(0)';
+    }, 1);
+};
+const closePostModal = () => {
+    MAIN.style.display = 'block';
+    MODAL_POST.style.transform = 'translateY(100vh)';
+};
